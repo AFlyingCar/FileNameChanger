952097419fd42e689a14b835d07fe96a0691ab14
Tyler Robbins <tyler@familyrobbins.com>
2014-02-14 08:56:26 -0800

    Renamed file 'foo2.py' to 'foo.py'. Added more variables.

--- foo.py
+++ foo.py
@@ -1,87 +1,34 @@
 import os
 import platform as plat
-
 if str(plat.system()) == "Windows":
 	slash = "\\"
 else:
 	slash = "//"
-
 loc = os.getcwd()
-num = 0
-
-print os.listdir(loc)
-
-#for files, dirs, root in os.listdir(loc):
-#	loc = os.getcwd()
-#	print files
-#	try:
-#		print files.split(".")
-#		ext = files.split(".")[0]
-#		os.chdir(loc + files) #check if item is a 
-#		print "here"
-#	except Exception:
-#		if files == "git":
-#			continue
-
-#		filename = "foo" + str(num) + "." + (ext)
-
-#		if files[:3] != "foo":
-#			print "foo " + files
-		
-#			try:
-#				os.renames(files, filename)
-#			except Exception:
-#				pass
-#		else:
-#			num += 1
-
-#for root, dirs, files in os.walk(loc):
-for item in os.listdir(loc):
-#	print dirs
-#	print root
-#	if dirs == ".git":
-#		continue
-	for file in item:
-		files += file
-	
+fileli = []
+num = -1
+work = True
+for root, dirs, files in os.walk(loc):
 	for file in files:
 		foo = file
-		if foo == ".git":
+		if loc + slash + ".git" in root:
 			continue
-		else:
-
-			ext = foo.split(".")[1]
-			print ext
-
-			filename = "foo" + str(num) + "." + str(ext)
-
-			if foo[:3] != "foo":
-				print "foo " + foo
-				os.renames(foo, filename)
-			else:
+		elif foo == "foo.py":
+			continue
+		elif foo[:3] == "foo":
+			continue
+		print dirs
+		print foo
+		print root
+		work = True
+		while work:
+			try:
 				num += 1
-
-nuclear = u'\u2622'
-
-Note = """>>> os.listdir(loc)
-['foo.txt', 'foo2.txt']
->>> for files in os.listdir(loc):
-	print files
-
-	
-foo.txt
-foo2.txt
->>> for files in os.listdir(loc):
-	print files
-	if files != "foo2.txt":
-		os.rename(files, "foo3.txt")
-
-		
-foo.txt
-foo2.txt
->>> print files
-foo2.txt
->>> for files in os.listdir(loc):
-	print files
-	if files != "foo2.txt":
-		os.rename(files, "foo3.txt")"""
+				ext = foo.split(".")[1]
+				filename = root + slash + "foo" + str(num) + "." + str(ext)
+				orig = root + slash + foo
+				os.rename(orig, filename)
+				work = False
+			except Exception:
+				print num
+				print file + ': exists'
