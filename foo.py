efb0722ea33a5100128cc9342bdf7c83848b960d
Tyler Robbins <tyler@familyrobbins.com>
2014-02-15 12:35:42 -0800

    Fixed infinite loop bug. Edited output.

--- foo.py
+++ foo.py
@@ -6,8 +6,10 @@ else:
 	slash = "//"
 loc = os.getcwd()
 fileli = []
+filec = {}
 num = -1
 work = True
+filenum = 0;
 for root, dirs, files in os.walk(loc):
 	for file in files:
 		foo = file
@@ -17,11 +19,9 @@ for root, dirs, files in os.walk(loc):
 			continue
 		elif foo[:3] == "foo":
 			continue
-		print dirs
-		print foo
-		print root
-		work = True
-		while work:
+		else:
+			print "File " + root + slash + foo
+			work = True
 			try:
 				num += 1
 				ext = foo.split(".")[1]
@@ -29,6 +29,15 @@ for root, dirs, files in os.walk(loc):
 				orig = root + slash + foo
 				os.rename(orig, filename)
 				work = False
+				filenum += 1
+				fileli.append(foo)
+				filec[foo] = "foo" + str(num) + "." + str(ext)
 			except Exception:
-				print num
-				print file + ': exists'
+				num += 1
+				print "> " + str(num)
+				print "> File " + root + slash + foo + ' exists.'
+print "\n" + str(filenum) + " files were changed."
+print "Files: "
+for item in fileli:
+	print "\t" + item + " -->",
+	print filec[item]
